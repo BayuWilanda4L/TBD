a8e6d7512bf4e6306a893a3611a123c949570ccf
Muhamad Bayu Wilanda <muhamadbayuwilanda@protonmail.com>
2016-12-25 10:42:37 +0700

    Update db_tbd.sql

--- db/db_tbd.sql
+++ db/db_tbd.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 22, 2016 at 06:36 PM
+-- Generation Time: Dec 25, 2016 at 10:41 AM
 -- Server version: 5.5.46-0ubuntu0.14.04.2
 -- PHP Version: 5.5.9-1ubuntu4.20
 
@@ -32,18 +32,24 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(50) NOT NULL,
   `nama_lengkap` varchar(50) NOT NULL,
   `ttl` varchar(15) NOT NULL,
-  `alamat` varchar(50) NOT NULL,
+  `provinsi` varchar(30) NOT NULL,
+  `kabupaten` varchar(30) NOT NULL,
+  `kecamatan` varchar(30) NOT NULL,
+  `kelurahan` varchar(30) NOT NULL,
+  `alamat_lengkap` text NOT NULL,
+  `kode_pos` varchar(10) NOT NULL,
+  `no_telp` varchar(20) NOT NULL,
   PRIMARY KEY (`email`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=34 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `password`, `nama_lengkap`, `ttl`, `alamat`) VALUES
-(10, 'bayu@gmail.com', '8df603b8c91605bb1fc7a692b4b00e6f281bd74c', 'Muhamad Bayu Wilanda', '2016-03-03', 'alamat palsu'),
-(11, 'hub@gmail.com', '0ba42f91902ccd115dd8f44a772602c3af4a592a', 'Huwala Huwala', '2016-03-03', 'Dimanapun');
+INSERT INTO `users` (`id`, `email`, `password`, `nama_lengkap`, `ttl`, `provinsi`, `kabupaten`, `kecamatan`, `kelurahan`, `alamat_lengkap`, `kode_pos`, `no_telp`) VALUES
+(32, 'bayuwilanda@protonmail.com', '3bcd05f7c46e54053f71651ff7f05e006fa74e7f', 'Muhamad bayu wilanda', '1996-06-22', 'Jawa Barat', 'Pangandaran', 'Parigi', 'Karang benda', 'Jl. Raya Cigugur No.578', '46393', '087725984564'),
+(33, 'ossas@gmail.com', 'b9f052500b7d77f42c58e1c633e17002f2672ca9', 'Ossas', '1943-06-22', 'Jawa Barat', 'Ciamis', 'Paraguay', 'Itulah', 'jauh', '46393', '085677654');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
